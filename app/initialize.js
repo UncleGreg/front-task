a0cdd4e1ac7f50a5d4302e37772387706791000b
unclegreg <grzesiekwitczak@gmail.com>
2017-10-12 20:44:29 +0200

    generating RSS feed from URL

--- app/initialize.js
+++ app/initialize.js
@@ -2,4 +2,31 @@ document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
 });
+
 var $ = require('jquery');
+
+$(document).ready(function() {
+	//feed to parse
+	var feed = "https://www.vg.no/rss/feed/forsiden/?frontId=1";
+
+	$.ajax(feed, {
+		accepts:{
+			xml:"application/rss+xml"
+		},
+		dataType:"xml",
+		success:function(data) {
+
+			$(data).find("item").each(function () {
+				var el = $(this);
+				var title = el.find("title").text()
+				var link = el.find("link").text()
+				var date = el.find("pubDate").text()
+
+				$( "#rss-feeds" ).append("<a class='feed-list__item' href='" + link + "' target='_blank'>" + "<div class='feed-list__date'>" + date + "</div>" + "<div class='feed-list__title'>" + title + "</div>" + "</a>");
+			});
+
+
+		}   
+	});
+
+});
